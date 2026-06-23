c34ab15548f17d4a86a99ae7ae880aa9a17ecd05
Rohit Kumar <72741501+rkumar49269@users.noreply.github.com>
2026-06-23 18:55:38 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.22%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        for(int i: {2,3,5}){
+            while(n%i == 0) n/=i;
+        }
+        return n==1;
+    }
+};
