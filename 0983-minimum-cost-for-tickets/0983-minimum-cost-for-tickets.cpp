b0a0734916fb6b26b9d59131b316c0653c8e6f55
Rohit Kumar <72741501+rkumar49269@users.noreply.github.com>
2026-06-28 01:41:17 -0700

    Time: 0 ms (100%), Space: 12.9 MB (90.39%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -22,6 +22,26 @@ public:
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         memset(dp, -1, sizeof(dp));
-        return solve(0, days, costs);
+        // return solve(0, days, costs);
+        int n = days.size();
+        unordered_set<int> st;
+        for(auto& i : days) st.insert(i);
+
+        dp[0] = 0;
+        int last_day = days[n-1];
+
+        for(int i=1; i<=last_day; i++){
+            if(st.find(i) == st.end()){
+                dp[i] = dp[i-1];
+                continue;
+            }
+
+            int cost_1 = costs[0] + dp[max(i-1, 0)];
+            int cost_7 = costs[1] + dp[max(i-7, 0)];
+            int cost_30 = costs[2] + dp[max(i-30, 0)];
+
+            dp[i] = min({cost_1, cost_7, cost_30});
+        }
+        return dp[last_day];
     }
 };
